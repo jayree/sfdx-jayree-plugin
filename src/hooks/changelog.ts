3977ef682412ef6d8c56439bdab854eb115927a2
jayree <jayree@users.noreply.github.com>
2019-10-05 15:54:46 +0200

    chore(hook): fix cachedir

--- src/hooks/changelog.ts
+++ src/hooks/changelog.ts
@@ -1,11 +1,12 @@
 import { Hook } from '@oclif/config';
 import { accessSync, readFileSync, writeFileSync } from 'fs';
+import { ensureDirSync } from 'fs-extra';
 import marked = require('marked');
 import terminalRenderer = require('marked-terminal');
 import { dirname, join } from 'path';
 
 // tslint:disable-next-line: no-any
-export const changelog: Hook<any> = async () => {
+export const changelog: Hook<any> = async function() {
   let currentPath = __dirname;
 
   let moduleRootPath;
@@ -24,22 +25,29 @@ export const changelog: Hook<any> = async () => {
 
   marked.setOptions({ renderer: new terminalRenderer() });
 
-  const latestversionfile = join(moduleRootPath, '.latestversion.json');
+  const cachedir = join(this.config.cacheDir, 'sfdx-jayree');
+  ensureDirSync(cachedir);
+  const versionfile = join(cachedir, 'version');
+
   const changelogfile = readFileSync(join(moduleRootPath, 'CHANGELOG.md'), 'utf8');
-  const packagejson = require(join(moduleRootPath, 'package.json'));
   let changelogtext;
 
+  const packagejson = JSON.parse(readFileSync(join(moduleRootPath, 'package.json'), 'utf8'));
+
   try {
-    const latestversion = require(latestversionfile);
+    const latestversion = JSON.parse(readFileSync(versionfile, 'utf8'));
     changelogtext = changelogfile.substring(0, changelogfile.indexOf(`[${latestversion.version}]`));
+    if (changelogtext.length === 0) {
+      throw new Error(`version not found`);
+    }
   } catch (err) {
     changelogtext = changelogfile.substring(0, changelogfile.indexOf('# [', 2));
   } finally {
     changelogtext = changelogtext.substring(0, changelogtext.lastIndexOf('\n'));
     if (changelogtext.length > 0) {
-      console.log(marked('# CHANGELOG (sfdx-jayree) - ' + moduleRootPath));
+      console.log(marked('# CHANGELOG (sfdx-jayree)'));
       console.log(marked(changelogtext));
     }
-    writeFileSync(latestversionfile, JSON.stringify({ version: packagejson.version }), 'utf8');
+    writeFileSync(versionfile, JSON.stringify({ version: packagejson.version }), 'utf8');
   }
 };
