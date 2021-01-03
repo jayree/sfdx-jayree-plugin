64d2d1dc89ec284448f681b3372364c9b1bbd290
jayree <jayree@users.noreply.github.com>
2021-01-04 00:56:17 +0100

    fix: fix changelog hook

--- src/hooks/changelog.ts
+++ src/hooks/changelog.ts
@@ -4,8 +4,8 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
+import * as fs from 'fs-extra';
 import { Hook } from '@oclif/config';
 import { ensureDirSync } from 'fs-extra';
 import marked = require('marked');
@@ -13,38 +13,39 @@ import terminalRenderer = require('marked-terminal');
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const changelog: Hook<any> = async function () {
-  marked.setOptions({ renderer: new terminalRenderer() });
+  process.once('exit', () => {
+    marked.setOptions({ renderer: new terminalRenderer() });
 
-  const cacheDir = join(this.config.cacheDir, 'sfdx-jayree');
-  ensureDirSync(cacheDir);
-  const versionFile = join(cacheDir, 'version');
+    const cacheDir = join(this.config.cacheDir, 'sfdx-jayree');
+    ensureDirSync(cacheDir);
+    const versionFile = join(cacheDir, 'version');
 
-  const moduleRootPath = join(__dirname, '/..', '/..', '/..');
-
-  try {
-    const changelogFile = readFileSync(join(moduleRootPath, 'CHANGELOG.md'), 'utf8');
-    const packageJson = JSON.parse(readFileSync(join(moduleRootPath, 'package.json'), 'utf8'));
-    let changelogText;
     try {
-      const latestVersion = JSON.parse(readFileSync(versionFile, 'utf8'));
-      changelogText = changelogFile.substring(0, changelogFile.indexOf(`[${latestVersion.version}]`));
-      if (changelogText.length === 0) {
-        throw new Error('version not found');
+      const moduleRootPath = join(__dirname, '..', '..', '..');
+      const changelogFile = fs.readFileSync(join(moduleRootPath, 'CHANGELOG.md'), 'utf8');
+      const packageJson = fs.readJSONSync(join(moduleRootPath, 'package.json'));
+      let changelogText;
+      try {
+        const latestVersion = fs.readJSONSync(versionFile);
+        changelogText = changelogFile.substring(0, changelogFile.indexOf(`[${latestVersion.version}]`));
+        if (changelogText.length === 0) {
+          throw new Error('version not found');
+        }
+      } catch (err) {
+        changelogText = changelogFile.substring(0, changelogFile.indexOf('# [', 2));
+      } finally {
+        changelogText = changelogText.substring(0, changelogText.lastIndexOf('\n'));
+        if (changelogText.length > 0) {
+          // eslint-disable-next-line no-console
+          console.log(marked('# CHANGELOG (sfdx-jayree)'));
+          // eslint-disable-next-line no-console
+          console.log(marked(changelogText));
+        }
+        fs.writeJsonSync(versionFile, { version: packageJson.version });
       }
-    } catch (err) {
-      changelogText = changelogFile.substring(0, changelogFile.indexOf('# [', 2));
-    } finally {
-      changelogText = changelogText.substring(0, changelogText.lastIndexOf('\n'));
-      if (changelogText.length > 0) {
-        // eslint-disable-next-line no-console
-        console.log(marked('# CHANGELOG (sfdx-jayree)'));
-        // eslint-disable-next-line no-console
-        console.log(marked(changelogText));
-      }
-      writeFileSync(versionFile, JSON.stringify({ version: packageJson.version }), 'utf8');
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log('CHANGELOG.md not found');
     }
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log('CHANGELOG.md not found');
-  }
+  });
 };
