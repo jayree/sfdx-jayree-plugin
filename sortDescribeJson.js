7d94782bb1e5598772a9f36762cf2b9c4405bd0f
jayree <jayree@users.noreply.github.com>
2021-06-09 17:16:43 +0200

    fix(gitdiff): update describe.json to match apiVersion

--- sortDescribeJson.js
+++ sortDescribeJson.js
@@ -1,11 +1,17 @@
 const fs = require('fs-extra');
-const xml2js = require('xml2js');
 
 (async () => {
   await fs.writeJson(
     'metadata/describe.json',
     {
-      metadataObjects: (await fs.readJSON('metadata/describe.json')).metadataObjects.sort(function (a, b) {
+      metadataObjects: (
+        await fs.readJSON(
+          require('path').join(
+            require('os').homedir(),
+            '/.local/share/sfdx/client/current/node_modules/salesforce-alm/metadata/describe.json'
+          )
+        )
+      ).metadataObjects.sort(function (a, b) {
         return a.xmlName < b.xmlName ? -1 : a.xmlName > b.xmlName ? 1 : 0;
       }),
     },
