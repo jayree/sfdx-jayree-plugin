2d29e08c0e6d35d33f1d0ade5de08b025122601b
jayree <jayree@users.noreply.github.com>
2021-06-22 00:03:42 +0200

    chore: update License

--- src/commands/jayree/automation/changeset/deploy.ts
+++ src/commands/jayree/automation/changeset/deploy.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, jayree
+ * Copyright (c) 2021, jayree
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
