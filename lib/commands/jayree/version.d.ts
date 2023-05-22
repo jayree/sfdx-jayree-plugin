24666c9c6203b1bedef1a33f8c8287b8b7d6d4de
jayree <jayree.git@icloud.com>
2023-05-22 02:58:06 +0200

    chore: remove commands from bundle

--- lib/commands/jayree/version.d.ts
+++
@@ -1,6 +0,0 @@
-import { SfCommand } from '@salesforce/sf-plugins-core';
-import { AnyJson } from '@salesforce/ts-types';
-export default class Version extends SfCommand<AnyJson> {
-    static readonly hidden = true;
-    run(): Promise<AnyJson>;
-}
